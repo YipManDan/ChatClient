cd9af1cfaf7edee1817fcfdfaf9a5f644d90f6cc
YipManDan <Yeh.DanielP@gmail.com>
2016-02-26 18:08:15 -0800

    Removed unnecessary GUI elements

--- src/ChatGUI.java
+++ src/ChatGUI.java
@@ -8,6 +8,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 
 /*
@@ -16,52 +17,30 @@ import java.awt.event.ActionListener;
 public class ChatGUI extends JFrame implements ActionListener {
 
     private static final long serialVersionUID = 1L;
-    // will first hold "Username:", later on "Enter message"
-    private JLabel label;
-    // to hold the Username and later on the messages
+    //Text field for user to enter message
     private JTextField tf;
-    // to hold the server address an the port number
-    private JTextField tfServer, tfPort;
-    // to Logout and get the list of the users
-    private JButton login, logout, whoIsIn;
     // for the chat room
     private JTextArea ta;
     // if it is for connection
     private boolean connected;
     // the Client object
     private ChatClient client;
-    // the default port number
-    private int defaultPort;
-    private String defaultHost;
+
+    private ArrayList<UserId> users;
 
     // Constructor connection receiving a socket number
-    ChatGUI(String host, int port) {
+    ChatGUI(ArrayList users) {
 
-        super("Chat Client");
-        defaultPort = port;
-        defaultHost = host;
+        super("Chat Box");
+
+        this.users = users;
 
         // The NorthPanel with:
         JPanel northPanel = new JPanel(new GridLayout(3,1));
-        // the server name anmd the port number
-        JPanel serverAndPort = new JPanel(new GridLayout(1,5, 1, 3));
-        // the two JTextField with default value for server address and port number
-        tfServer = new JTextField(host);
-        tfPort = new JTextField("" + port);
-        tfPort.setHorizontalAlignment(SwingConstants.RIGHT);
-
-        serverAndPort.add(new JLabel("Server Address:  "));
-        serverAndPort.add(tfServer);
-        serverAndPort.add(new JLabel("Port Number:  "));
-        serverAndPort.add(tfPort);
-        serverAndPort.add(new JLabel(""));
-        // adds the Server an port field to the GUI
-        northPanel.add(serverAndPort);
 
         // the Label and the TextField
-        label = new JLabel("Enter your username below", SwingConstants.CENTER);
-        northPanel.add(label);
-        tf = new JTextField("Anonymous");
+        northPanel.add(new JLabel("Enter your message below:", SwingConstants.CENTER));
+        tf = new JTextField("");
         tf.setBackground(Color.WHITE);
         northPanel.add(tf);
         add(northPanel, BorderLayout.NORTH);
@@ -74,19 +53,8 @@ public class ChatGUI extends JFrame implements ActionListener {
         add(centerPanel, BorderLayout.CENTER);
 
         // the 3 buttons
-        login = new JButton("Login");
-        login.addActionListener(this);
-        logout = new JButton("Logout");
-        logout.addActionListener(this);
-        logout.setEnabled(false);		// you have to login before being able to logout
-        whoIsIn = new JButton("Who is in");
-        whoIsIn.addActionListener(this);
-        whoIsIn.setEnabled(false);		// you have to login before being able to Who is in
 
         JPanel southPanel = new JPanel();
-        southPanel.add(login);
-        southPanel.add(logout);
-        southPanel.add(whoIsIn);
         add(southPanel, BorderLayout.SOUTH);
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -101,6 +69,7 @@ public class ChatGUI extends JFrame implements ActionListener {
         ta.append(str);
         ta.setCaretPosition(ta.getText().length() - 1);
     }
+    /*
     // called by the GUI is the connection failed
     // we reset our buttons, label, textfield
     void connectionFailed() {
@@ -119,6 +88,7 @@ public class ChatGUI extends JFrame implements ActionListener {
         tf.removeActionListener(this);
         connected = false;
     }
+    */
 
     /*
     * Button or JTextField clicked
@@ -126,16 +96,6 @@ public class ChatGUI extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         Object o = e.getSource();
-        // if it is the Logout button
-        if(o == logout) {
-            client.sendMessage(new ChatMessage(ChatMessage.LOGOUT, "", client.getSelf()));
-            return;
-        }
-        // if it the who is in button
-        if(o == whoIsIn) {
-            client.sendMessage(new ChatMessage(ChatMessage.WHOISIN, "", client.getSelf()));
-            return;
-        }
 
         // ok it is coming from the JTextField
         if(connected) {
@@ -144,60 +104,11 @@ public class ChatGUI extends JFrame implements ActionListener {
             tf.setText("");
             return;
         }
-
-
-        if(o == login) {
-            // ok it is a connection request
-            String username = tf.getText().trim();
-            // empty username ignore it
-            if(username.length() == 0)
-                return;
-            
-            // empty serverAddress ignore it
-            String server = tfServer.getText().trim();
-            if(server.length() == 0)
-                return;
-            
-            // empty or invalid port numer, ignore it
-            String portNumber = tfPort.getText().trim();
-            if(portNumber.length() == 0)
-                return;
-            
-            int port = 0;
-            
-            try {
-                port = Integer.parseInt(portNumber);
-            } catch(Exception en) {
-                return;   // nothing I can do if port number is not valid
-            }
-
-            // try creating a new Client with GUI
-            //client = new ChatClient(server, port, username, this);
-            // test if we can start the Client
-            if(!client.start()) 
-                return;
-            
-            tf.setText("");
-            label.setText("Enter your message below");
-            connected = true;
-
-            // disable login button
-            login.setEnabled(false);
-            // enable the 2 buttons
-            logout.setEnabled(true);
-            whoIsIn.setEnabled(true);
-            // disable the Server and Port JTextField
-            tfServer.setEditable(false);
-            tfPort.setEditable(false);
-            // Action listener for when the user enter a message
-            tf.addActionListener(this);
-        }
-
     }
 
     // to start the whole thing the server
     public static void main(String[] args) {
-        //new ChatGUI("localhost", 8080);
+        new ChatGUI(null);
     }
 
 }
